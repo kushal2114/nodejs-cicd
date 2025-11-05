524d91554d767174d04fd5b4fa1150c33c4e3340
kushal2114 <ramwanikushal@gmail.com>
2025-11-05 11:58:42 +0530

    Modified index.js to test CI pipline

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, CI/CD  with Node.js");
+    res.send("Hello Kushal, This is CI/CD  with Node.js");
 });
 
 const PORT = process.env.PORT || 3000;
